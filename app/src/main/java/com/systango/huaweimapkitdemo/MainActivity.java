cb3c394f4dd665bb4258e8f0f9ffeeeb4d608097
santiagoluisfina <santiagofina@gmail.com>
2020-08-23 19:14:37 +0200

    lo hice andar!!!

--- app/src/main/java/com/systango/huaweimapkitdemo/MainActivity.java
+++ app/src/main/java/com/systango/huaweimapkitdemo/MainActivity.java
@@ -1,42 +1,154 @@
 package com.systango.huaweimapkitdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
-
+import com.huawei.hms.maps.CameraUpdate;
+import com.huawei.hms.maps.CameraUpdateFactory;
 import com.huawei.hms.maps.HuaweiMap;
 import com.huawei.hms.maps.MapView;
+import com.huawei.hms.maps.MapsInitializer;
 import com.huawei.hms.maps.OnMapReadyCallback;
+import com.huawei.hms.maps.model.BitmapDescriptorFactory;
+import com.huawei.hms.maps.model.CameraPosition;
+import com.huawei.hms.maps.model.Circle;
+import com.huawei.hms.maps.model.CircleOptions;
+import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.Marker;
+import com.huawei.hms.maps.model.MarkerOptions;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+/**
+ * map activity entrance class
+ */
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private HuaweiMap hMap;
-    private MapView mapView;
+    private static final String TAG = "MapViewDemoActivity";
+
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+    private static final int REQUEST_CODE = 100;
+
+    private static final LatLng LAT_LNG = new LatLng(31.2304, 121.4737);
+
+    private HuaweiMap hmap;
+
+    private MapView mMapView;
+
+    private Marker mMarker;
+
+    private Circle mCircle;
+
+    private static final String[] RUNTIME_PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "map onCreate:");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mapView = findViewById(R.id.mapView);
-        mapView.getMapAsync(this);
+        if (!hasPermissions(this, RUNTIME_PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, RUNTIME_PERMISSIONS, REQUEST_CODE);
+        }
+
+        // get mapView by layout view
+        mMapView = findViewById(R.id.mapView);
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        // please replace "Your API key" with api_key field value in
+        // agconnect-services.json if the field is null.
+        MapsInitializer.setApiKey("CgB6e3x9M4kVO6PgHcu8SnAOVjEnql4rMWRUhyesI5bd1yXVKw2dwmW9DliQ7R/ecoJVheSAQXxpmRLKPsNmX9xU");
+        mMapView.onCreate(mapViewBundle);
+
+        // get map by async method
+        mMapView.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(HuaweiMap huaweiMap) {
-        huaweiMap.setMyLocationEnabled(true);
-        huaweiMap.getUiSettings().setCompassEnabled(true);
-        huaweiMap.getUiSettings().setRotateGesturesEnabled(true);
-        huaweiMap.getUiSettings().setScrollGesturesEnabled(true);
-        huaweiMap.getUiSettings().setScrollGesturesEnabledDuringRotateOrZoom(true); 
-        huaweiMap.getUiSettings().setTiltGesturesEnabled (true);
-        huaweiMap.getUiSettings().setZoomControlsEnabled (true);
-        huaweiMap.getUiSettings().setIndoorLevelPickerEnabled (true);
-        huaweiMap.getUiSettings().setMapToolbarEnabled (true);
-        huaweiMap.getUiSettings().setZoomGesturesEnabled(false);
-        huaweiMap.getUiSettings().setAllGesturesEnabled(true);
-                
-        hMap = huaweiMap;
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onMapReady(HuaweiMap map) {
+        Log.d(TAG, "onMapReady: ");
+
+        // after call getMapAsync method ,we can get HuaweiMap instance in this call back method
+        hmap = map;
+        hmap.setMyLocationEnabled(true);
+
+        LatLng romaLatLng = new LatLng(41.902782, 12.496366);
+
+        // move camera by CameraPosition param ,latlag and zoom params can set here
+        //CameraPosition build = new CameraPosition.Builder().target(new LatLng(60, 60)).zoom(5).build();
+        CameraPosition build = new CameraPosition.Builder().target(romaLatLng).zoom(5).build();
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(build);
+        hmap.animateCamera(cameraUpdate);
+        hmap.setMaxZoomPreference(5);
+        hmap.setMinZoomPreference(2);
+
+        // mark can be add by HuaweiMap
+        mMarker = hmap.addMarker(new MarkerOptions().position(romaLatLng).title("Marcatore in Roma")
+                .clusterable(true));
+
+        mMarker.showInfoWindow();
+
+        // circle can be add by HuaweiMap
+        mCircle = hmap.addCircle(new CircleOptions().center(new LatLng(60, 60)).radius(5000).fillColor(Color.GREEN));
+        mCircle.setFillColor(Color.TRANSPARENT);
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
